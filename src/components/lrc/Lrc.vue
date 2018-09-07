360dc2cc1b931598fe96209568e1b79d6e861076
XIAO小恋歌 <353673246@qq.com>
2018-09-07 20:22:35 +0800

    lrc

--- src/components/lrc/Lrc.vue
+++ src/components/lrc/Lrc.vue
@@ -1,3 +1,11 @@
 <template>
-  <div>lrc</div>
+  <div class = "lrc">歌词暂无</div>
 </template>
+<style lang="scss">
+.lrc{
+  width: 100%;
+  text-align:center;
+  font-size: 10vw;
+  margin-top: 10vw;
+}
+</style>
