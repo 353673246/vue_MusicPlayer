510a91f8d80c8057ceed30550bddd41325e9c832
XIAO小恋歌 <353673246@qq.com>
2018-09-08 23:46:12 +0800

    lrc second

--- src/components/lrc/Lrc.vue
+++ src/components/lrc/Lrc.vue
@@ -1,11 +1,33 @@
 <template>
-  <div class = "lrc">歌词暂无</div>
+  <div class = "lrc" ref="div">
+    <ul>
+      <li v-for="(item,key) in parseLyric" :key="key">{{item}}</li>
+    </ul>
+  </div>
 </template>
+<script>
+import {mapGetters, mapState} from 'vuex'
+export default {
+  computed: {
+    ...mapGetters('list', ['currentItem', 'getLrc', 'parseLyric']),
+    ...mapState('player', ['currentTime'])
+  },
+  mounted () {
+    // console.log(this.parseLyric)
+    console.log(this.currentTime)
+    for (let i in this.parseLyric) {
+      console.log(i)
+    }
+  }
+}
+</script>
+
 <style lang="scss">
 .lrc{
   width: 100%;
   text-align:center;
-  font-size: 10vw;
+  font-size: 4vw;
   margin-top: 10vw;
+  font-weight: bold;
 }
 </style>
--- src/store/list/getters.js
+++ src/store/list/getters.js
@@ -1,5 +1,26 @@
 export default {
   currentItem (state) {
     return state.musicList[state.currentIndex]
+  },
+  parseLyric (state) {
+    let lrc = state.musicList[state.currentIndex].lrc
+    let lyrics = lrc.split('\n')
+    // console.log(lyrics)
+    let lrcObj = {}
+    for (let i = 0; i < lyrics.length; i++) {
+      let lyric = decodeURIComponent(lyrics[i])
+      let timeReg = /\[\d*:\d*((\.|:)\d*)*\]/g
+      let timeRegExpArr = lyric.match(timeReg)
+      if (!timeRegExpArr) continue
+      let clause = lyric.replace(timeReg, '')
+      for (let k = 0, h = timeRegExpArr.length; k < h; k++) {
+        let t = timeRegExpArr[k]
+        let min = Number(String(t.match(/\[\d*/i)).slice(1))
+        let sec = Number(String(t.match(/:\d*/i)).slice(1))
+        let time = min * 60 + sec
+        lrcObj[time] = clause
+      }
+    }
+    return lrcObj
   }
 }
