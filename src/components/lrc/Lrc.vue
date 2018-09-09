252b6d0e72dfb3b1adf1480edd1586de8aaf9e06
XIAO小恋歌 <353673246@qq.com>
2018-09-09 10:52:36 +0800

    vue-music

--- src/components/lrc/Lrc.vue
+++ src/components/lrc/Lrc.vue
@@ -1,23 +1,51 @@
 <template>
   <div class = "lrc" ref="div">
     <ul>
-      <li v-for="(item,key) in parseLyric" :key="key">{{item}}</li>
+      <li v-for="(item,key) in parseLyric" :key="key" ref="li" >{{item}}</li>
     </ul>
   </div>
 </template>
 <script>
 import {mapGetters, mapState} from 'vuex'
 export default {
+  name: 'lrc',
   computed: {
-    ...mapGetters('list', ['currentItem', 'getLrc', 'parseLyric']),
+    ...mapGetters('list', ['currentItem', 'parseLyric']),
     ...mapState('player', ['currentTime'])
+  },
+  methods: {
+    getLrc () {
+      // console.log(lrc)
+      // this.lrc.map((item, i) => {
+
+      // })
+      let li = document.querySelector('div ul li')
+      for (let i in this.parseLyric) {
+        console.log(parseInt(i))
+        console.log('-')
+        console.log(this.currentTime)
+        if (parseInt(i) === this.currentTime) {
+          li.className = 'active'
+          // this.$refs.li.classList.add('active')
+          // console.log('1')
+        }
+      }
+    }
   },
   mounted () {
     // console.log(this.parseLyric)
-    console.log(this.currentTime)
-    for (let i in this.parseLyric) {
-      console.log(i)
-    }
+    this.getLrc()
+    // let list = this.parseLyric
+    // let curTime = this.currentTime
+    // let lrclist = list.map((item, i) => {
+    //   console.log(curTime)
+    //   console.log(lrclist)
+    // })
+    // console.log(this.parseLyric)
+    // console.log(this.currentTime)
+    // for (let i in this.parseLyric) {
+    // console.log(i)
+    // }
   }
 }
 </script>
@@ -29,5 +57,8 @@ export default {
   font-size: 4vw;
   margin-top: 10vw;
   font-weight: bold;
+  .active{
+    color: red;
+  }
 }
 </style>
